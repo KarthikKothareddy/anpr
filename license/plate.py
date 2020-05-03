7582383efc6bbd502502afdcac7df33c6e97d7fa
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-05-03 11:16:20 -0500

    finalized localizing plates in images

--- license/plate.py
+++ license/plate.py
@@ -18,18 +18,74 @@ class LicensePlateDetector:
         return self.detect_plates()
 
     def detect_plates(self):
-
         # init rectangle kernel
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
         # init square kernel
         square_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        regions = []
+
         # to grayscale
         gray = cv2.cvtColor(self.image, self._to_gray)
-        # apply the blackhat operation
+        # apply blackhat to reveal darker regions on light background
         blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rect_kernel)
 
-        return blackhat
+        # find regions in the image that are light
+        # apply morphology CLOSE
+        light = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, square_kernel)
+        # threshold the image between 50 and 255 because our target plates are mostly white in color
+        light = cv2.threshold(light, 50, 255, cv2.THRESH_BINARY)[1]
+
+        # compute the Scharr gradient representation of the blackhat image in the x-direction
+        grad_x = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+        grad_x = np.absolute(grad_x)
+        min_val, max_val = (np.min(grad_x), np.max(grad_x))
+        # min/max scaling to [0, 255]
+        grad_x = (255 * ((grad_x - min_val) / (max_val - min_val))).astype("uint8")
+
+        # blur the gradient representation,
+        grad_x = cv2.GaussianBlur(grad_x, (5, 5), 0)
+        # apply a morphology CLOSE
+        grad_x = cv2.morphologyEx(grad_x, cv2.MORPH_CLOSE, rect_kernel)
+        # threshold the image using Otsu's method
+        thresh = cv2.threshold(grad_x, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        # perform a series of erosions and dilations on the image
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # take the bitwise 'and' between the 'light' regions of the image
+        thresh = cv2.bitwise_and(thresh, thresh, mask=light)
+        # another series of erosions and dilations
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        thresh = cv2.erode(thresh, None, iterations=1)
+
+        # find contours in the thresholded image
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        regions = []
+        # loop over the contours
+        for c in cnts:
+            # grab the bounding box associated with the contour and compute the area and
+            # aspect ratio
+            (w, h) = cv2.boundingRect(c)[2:]
+            aspect_ratio = w / float(h)
+
+            # calculate *extent* for additional filtering
+            shape_area = cv2.contourArea(c)
+            bbox_area = w * h
+            extent = shape_area / float(bbox_area)
+            extent = int(extent * 100) / 100
+
+            # compute the rotated bounding box of the region
+            rect = cv2.minAreaRect(c)
+            box = np.int0(cv2.boxPoints(rect))
+
+            # ensure the aspect ratio, width, and height of the bounding box fall within
+            # tolerable limits, then update the list of license plate regions
+            if (3 < aspect_ratio < 6) and h > self.min_plate_h and w > self.min_plate_w and extent > 0.50:
+                regions.append(box)
+
+        # return the list of license plate regions
+        return regions
 
 
 
--- recognize.py
+++ recognize.py
@@ -2,16 +2,37 @@
 
 # contains all the test runs
 import cv2
+import imutils
+import random
+from imutils import paths
+import numpy as np
 from utils.display_utils import display
 from license.plate import LicensePlateDetector
 
 
-# initialize a plate detector
-detector = LicensePlateDetector(
-    image=cv2.imread("sample.jpg")
-)
+def localize_and_draw(image_path):
+    # load the image
+    image = cv2.imread(image_path)
+    print(image_path)
 
-modified = detector.detect()
+    # if the width is greater than 640 pixels, then resize the image
+    image = imutils.resize(image, width=640) if image.shape[1] > 640 else image
 
-# show modified
-display(modified, flip=False)
+    # initialize the license plate detector and detect the license plates and characters
+    lpd = LicensePlateDetector(image)
+    plates = lpd.detect()
+
+    # loop over the license plate regions and draw the bounding box surrounding the
+    # license plate
+    for lp_box in plates:
+        lp_box = np.array(lp_box).reshape((-1, 1, 2)).astype(np.int32)
+        cv2.drawContours(image, [lp_box], -1, (0, 255, 0), 2)
+    # show
+    display(image)
+
+
+paths = random.sample(list(paths.list_images("./data")), 2)
+for path in paths:
+    localize_and_draw(path)
+
+
